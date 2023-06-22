0c55a3ca8384ca08249949e9d717bf7f18789a11
Vinicius Noli <viniciusnoli2016@gmail.com>
2023-06-22 14:10:44 -0300

    feat: rest operator

--- applying/dist/app.js
+++ applying/dist/app.js
@@ -1,4 +1,14 @@
 "use strict";
-(() => {
-    console.log('Running one..');
-})();
+const add = (...numbers) => {
+    return numbers.reduce((curResult, curValue) => {
+        return curResult + curValue;
+    }, 0);
+};
+const sum = add(10.59, 1.2, 9, 0, 3);
+const person = {
+    firstName: 'Vinicius',
+    age: 21
+};
+// nomes de constantes precisam ser as mesmas que as
+// propriedades
+const { firstName, age } = person;
